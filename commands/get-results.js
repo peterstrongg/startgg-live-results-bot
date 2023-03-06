e24b4036b6c03ab186b2941558ad3dfa50b4c0f4
peterstrongg <peterstrong8343@gmail.com>
2023-03-06 14:37:33 -0500

    get-results args implementation fully functional

--- commands/get-results.js
+++ commands/get-results.js
@@ -5,9 +5,23 @@ const { getSets } = require("../api/getSets");
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('getresults')
-		.setDescription('Returns results of a startgg bracket'),
+		.setDescription('Returns results of a startgg bracket')
+		.addStringOption(option =>
+			option.setName('tournamentname')
+				.setDescription('Name of tournament')
+				.setRequired(true)
+		)
+		.addStringOption(option =>
+			option.setName('eventname')
+				.setDescription('Name of event')
+				.setRequired(true)
+		),
 	async execute(interaction) {
-		let id = await getEventId("bair-trap-23-air-jordan", "ultimate-singles").then(response => {return response.event.id});
+		const tournamentName = interaction.options.getString('tournamentname');
+		const eventName = interaction.options.getString('eventname');
+
+		let id = await getEventId(tournamentName, eventName).then(response => {return response.event.id});
+		
 		let results = await getSets(id).then(response => {
 			let res = response.event.standings.nodes;
 			let resultsString = "";
